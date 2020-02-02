7658d06eff14059b123857d3501fd26112bd8cf5
justinaray <justinaray828@gmail.com>
2020-02-02 14:12:47 -0500

    - Win scene has been created

---
+++ Assets/WinFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinFade : MonoBehaviour
+{
+    public SceneHandler sceneHandler;
+
+    private Image image;
+    private float alpha = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha += Time.deltaTime;
+        image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+
+        if(alpha >= 2)
+        {
+            sceneHandler.LoadNextScene();
+        }
+    }
+}
